8fa1120ad7e5bdf891f83566a329ddfaddeca13c
Thomas Tilahun <thomm.sef@gmail.com>
2020-07-31 21:08:16 -0700

    handeled error

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect, Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import * as Permissions from "expo-permissions";
 
 import * as Location from "expo-location";
@@ -14,8 +14,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: 0,
-      lon: 0,
+      latitude: 0,
+      longitude: 0,
       error: null,
     };
   }
@@ -24,8 +24,8 @@ export default class App extends Component {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
-          latitude: location.coords.lat,
-          longitude: location.coords.lon,
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
           error: null,
         });
       },
@@ -40,15 +40,16 @@ export default class App extends Component {
         <MapView
           style={styles.map}
           region={{
-            latitude: this.state.lat,
-            longitude: this.state.lon,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.014,
+            longitudeDelta: 0.012,
           }}
         >
           <Marker coordinate={this.state} />
         </MapView>
       </View>
     );
-    <Footer />;
   }
 }
 const styles = StyleSheet.create({
@@ -59,49 +60,3 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
-
-// export default class App extends Component {
-
-//   super(props) {
-
-//     this.satate = {
-//       lat: 0,
-//       lon: 0,
-//     };
-//   }
-
-//   componet
-
-// }
-
-// export default function App() {
-//   // const { permissions, setPermissions } = useState(false);
-//   const [location, setLocation] = useState({});
-//   const [lat, setLat] = useState();
-//   const [long, setLong] = useState();
-//   useEffect(() => {
-//     (async () => {
-//       let { status } = await Location.requestPermissionsAsync();
-//       if (status !== "granted") {
-//         setErrorMsg("Permission to access location was denied");
-//       }
-
-//       let location = await Location.getCurrentPositionAsync({});
-//       setLocation(location.coords);
-//       setLat(location.coords.latitude.toFixed(4));
-//       setLong(location.coords.longitude.toFixed(4));
-//     })();
-//   }, []);
-
-//   let text = "Waiting..";
-//   // if (errorMsg) {
-//   //   text = errorMsg;
-//   // } else
-//   if (location) {
-//     text = JSON.stringify(location);
-//   }
-
-//   console.log("LOCATION", location);
-//   console.log("LATITUDE", lat);
-//   console.log("LONGITUDE", long);
-//   console.log("URL", url);
